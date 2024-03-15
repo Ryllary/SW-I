8f088080331d4c84c7640ed13c14f2400f509032
Ryll Barroso <124801279+Ryllary@users.noreply.github.com>
2024-03-15 12:44:57 -0300

    Add files via upload

---
+++ Projetos/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo ao banco da etec MCM!");
+
+            //Instanciar a classe da Conta e criar um objeto dela
+            Conta conta = new Conta();
+
+            conta.consulta_saldo();
+
+            conta.depositar(1000.35);
+            conta.consulta_saldo();
+
+            conta.sacar(500);
+            consulta.saldo();
+          
+        }
+    }
+}
